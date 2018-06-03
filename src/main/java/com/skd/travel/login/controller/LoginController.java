9f1be4f561b783b564836ba0e6fa8fc8a3506e89
SUDHIR Dubey <sudhirdubey1990@gmail.com>
2018-06-03 17:21:01 +0530

    added gitignore

--- src/main/java/com/skd/travel/login/controller/LoginController.java
+++ src/main/java/com/skd/travel/login/controller/LoginController.java
@@ -4,14 +4,12 @@ import com.skd.travel.login.model.User;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("v1//users")
 public class LoginController {
+    @GetMapping("/")
     public HttpEntity<String> ping() {
         return new ResponseEntity<>("I am LoginService", HttpStatus.OK);
     }
